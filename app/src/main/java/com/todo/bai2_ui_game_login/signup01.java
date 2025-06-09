939ec0e673620087a39ce5519567744e4bcbe69c
TaiLe <lephuoctai2006@gmail.com>
2025-06-09 23:35:17 +0700

    backup 4
    layout UI sign up

--- app/src/main/java/com/todo/bai2_ui_game_login/signup01.java
+++ app/src/main/java/com/todo/bai2_ui_game_login/signup01.java
@@ -1,14 +1,43 @@
 package com.todo.bai2_ui_game_login;
 
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.ArrayList;
+
 public class signup01 extends AppCompatActivity {
+    private void linkText(View viewText, String src, ArrayList<String> spans, int idColor, View.OnClickListener listener) {
+        MaterialTextView terms = (MaterialTextView) viewText;
+        SpannableString spannableString = new SpannableString(src);
+        int color = ContextCompat.getColor(this, idColor);
+
+        for(String span : spans) {
+            int start = src.indexOf(span);
+            int end = start + span.length();
+
+            spannableString.setSpan(
+                    new ForegroundColorSpan(color),
+                    start,
+                    end,
+                    spannableString.SPAN_EXCLUSIVE_EXCLUSIVE
+            );
+        };
+
+        terms.setOnClickListener(listener);
+        terms.setText(spannableString);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +49,13 @@ public class signup01 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView viewText = findViewById(R.id.signup_terms);
+        String src = viewText.getText().toString();
+        ArrayList<String> spans = new ArrayList<>();
+        spans.add("Terms of Service");
+        spans.add("Privacy Policy");
+        int idColor = R.color.orange800;
+
+        linkText((View)viewText,src, spans, idColor, (View v) -> {} );
     }
 }
